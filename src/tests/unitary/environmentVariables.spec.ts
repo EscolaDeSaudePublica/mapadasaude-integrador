e46a387c8bfa272ef01ab0f1365d970127c88d72
ericsonmoreira <moreira.ericson@gmail.com>
2021-08-12 12:29:37 -0300

    add test de variáveis de ambiente do SACS #439

--- src/tests/unitary/environmentVariables.spec.ts
+++ src/tests/unitary/environmentVariables.spec.ts
@@ -3,4 +3,9 @@ describe('Unitário - Variáveis de ambiente', () => {
     expect(process.env.MAPAS_URL).not.toBeNull();
     expect(process.env.MAPAS_JWT).not.toBeNull();
   });
+
+  test('Deve ser possível acessar as variáveis de ambiente da api SACS', async () => {
+    expect(process.env.SACS_URL).not.toBeNull();
+    expect(process.env.SACS_JWT).not.toBeNull();
+  });
 });
